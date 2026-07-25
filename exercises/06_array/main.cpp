ae34f2764b68d0c0e573de68b28c5b6471b907de
111hyq111 <167197286+111hyq111@users.noreply.github.com>
2026-07-25 17:23:38 +0800

    array相关操作+unsigned long long字节长度+三目表达式

--- exercises/06_array/main.cpp
+++ exercises/06_array/main.cpp
@@ -2,6 +2,7 @@
 
 // READ: 数组 <https://zh.cppreference.com/w/cpp/language/array>
 
+//显式初始化前两个元素：arr[0] = 0，arr[1] = 1。  其余元素自动初始化为 0
 unsigned long long arr[90]{0, 1};
 unsigned long long fibonacci(int i) {
     switch (i) {
@@ -11,13 +12,17 @@ unsigned long long fibonacci(int i) {
             return 1;
         default:
             // TODO: 补全三目表达式缺失的部分
-            return <condition> ? <cache> : (arr[i] = fibonacci(i - 1) + fibonacci(i - 2));
+            //检查 arr[i] 是否非零（即是否已缓存计算结果）
+            //若已缓存，直接返回缓存值。 
+            //否则：递归计算，将结果存入 arr[i]，并返回该值。
+            return arr[i] ? arr[i] : (arr[i] = fibonacci(i - 1) + fibonacci(i - 2));
     }
 }
 
 int main(int argc, char **argv) {
     // TODO: 为此 ASSERT 填写正确的值
-    ASSERT(sizeof(arr) == ?, "sizeof array is size of all its elements");
+    //arr 有 90 个元素，每个 unsigned long long 为 8 字节
+    ASSERT(sizeof(arr) == 720, "sizeof array is size of all its elements");
     // ---- 不要修改以下代码 ----
     ASSERT(fibonacci(2) == 1, "fibonacci(2) should be 1");
     ASSERT(fibonacci(20) == 6765, "fibonacci(20) should be 6765");
