365a3a2ec65dfa596bd834c9d1f9d04c6399cc1e
111hyq111 <167197286+111hyq111@users.noreply.github.com>
2026-07-27 21:15:36 +0800

    std_array的用法

--- exercises/24_std_array/main.cpp
+++ exercises/24_std_array/main.cpp
@@ -8,21 +8,30 @@
 int main(int argc, char **argv) {
     {
         std::array<int, 5> arr{{1, 2, 3, 4, 5}};
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
+        ASSERT(arr.size() == 5, "Fill in the correct value.");
+        //int型 字节数为4
+        ASSERT(sizeof(arr) == 20, "Fill in the correct value.");
         int ans[]{1, 2, 3, 4, 5};
-        ASSERT(std::memcmp(arr.?, ans, ?) == 0, "Fill in the correct values.");
+        //std::memcmp  第一个参数：指向第一块内存的指针
+        //             第二个参数：指向第二块内存的指针
+        //             第三个参数：要比较的字节数。
+        //arr.data()返回指向首元素的指针
+        ASSERT(std::memcmp(arr.data(), ans, sizeof(ans)) == 0, "Fill in the correct values.");
     }
     {
         std::array<double, 8> arr;
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
+        ASSERT(arr.size() == 8, "Fill in the correct value.");
+        //double型 字节数为8
+        ASSERT(sizeof(arr) == 64, "Fill in the correct value.");
     }
     {
         std::array<char, 21> arr{"Hello, InfiniTensor!"};
-        ASSERT(arr.size() == ?, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == ?, "Fill in the correct value.");
-        ASSERT(std::strcmp(arr.?, "Hello, InfiniTensor!") == 0, "Fill in the correct value.");
+        ASSERT(arr.size() == 21, "Fill in the correct value.");
+        //char型 字节数为1
+        ASSERT(sizeof(arr) == 21, "Fill in the correct value.");
+        //std::strcmp   第一个参数：指向第一个字符串（左侧）的指针
+        //              第二个参数：指向第二个字符串（右侧）的指针
+        ASSERT(std::strcmp(arr.data(), "Hello, InfiniTensor!") == 0, "Fill in the correct value.");
     }
     return 0;
 }
